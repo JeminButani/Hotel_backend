98d9aabf5f09d04559e8d0209763fb8b9ad27595
JeminButani <jdbutani2002@gmail.com>
2023-03-07 13:58:41 +0530

    Third Commit

--- server.js
+++ server.js
@@ -25,48 +25,48 @@ app.use(cors());
 const port = 5000;
 
 // to get all the data
-// app.get("/totalrooms", async (req, res) => {
-//   try {
-//     const allRooms = [];
-//     const available = await available_rooms.find({});
-//     const booked = await booked_rooms.find({});
-//     available.forEach((e) => {
-//       allRooms.push(e);
-//     });
-//     booked.forEach((e) => {
-//       allRooms.push(e);
-//     });
+app.get("/totalrooms", async (req, res) => {
+  try {
+    const allRooms = [];
+    const available = await available_rooms.find({});
+    const booked = await booked_rooms.find({});
+    available.forEach((e) => {
+      allRooms.push(e);
+    });
+    booked.forEach((e) => {
+      allRooms.push(e);
+    });
 
-//     res.send({ status: "ok", data: allRooms });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// });
+    res.send({ status: "ok", data: allRooms });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 // get the filtered data
-// app.post("/findRooms", async (req, res) => {
-//   const type = String(req.body.type);
-//   const beds = Number(req.body.beds);
-//   const date = new Date(req.body.date);
+app.post("/findRoomsNumber", async (req, res) => {
+  const type = String(req.body.type);
+  const beds = Number(req.body.beds);
+  const date = new Date(req.body.date);
 
-//   const availableRooms = [];
+  const availableRooms = [];
 
-//   const booked = await booked_rooms.find({
-//     $or: [{ sdate: { $lte: date } }, { edate: { $gte: date } }],
-//     type: type,
-//     beds: beds,
-//   });
+  const booked = await booked_rooms.find({
+    $or: [{ sdate: { $lte: date } }, { edate: { $gte: date } }],
+    type: type,
+    beds: beds,
+  });
 
-//   const available = await available_rooms.find({ type: type, beds: beds });
-//   available.forEach((e) => {
-//     availableRooms.push(e);
-//   });
-//   booked.forEach((e) => {
-//     availableRooms.push(e);
-//   });
+  const available = await available_rooms.find({ type: type, beds: beds });
+  available.forEach((e) => {
+    availableRooms.push(e);
+  });
+  booked.forEach((e) => {
+    availableRooms.push(e);
+  });
 
-//   res.send("Rooms - " + availableRooms);
-// });
+  res.send("Rooms - " + availableRooms);
+});
 
 // new function according to the required modification
 app.post("/findRooms", async (req, res) => {
@@ -114,76 +114,76 @@ app.post("/findRooms", async (req, res) => {
 });
 
 // temp for adding data to the booked rooms for testing
-// app.post("/addData", async (req, res) => {
-//   const rno = Number(req.body.rno);
-//   const type = String(req.body.type);
-//   const beds = Number(req.body.beds);
-//   const sdate = new Date(req.body.sdate);
-//   const edate = new Date(req.body.edate);
+app.post("/addData", async (req, res) => {
+  const rno = Number(req.body.rno);
+  const type = String(req.body.type);
+  const beds = Number(req.body.beds);
+  const sdate = new Date(req.body.sdate);
+  const edate = new Date(req.body.edate);
 
-//   const result = await booked_rooms.create({
-//     rno: rno,
-//     type: type,
-//     beds: beds,
-//     sdate: sdate,
-//     edate: edate,
-//   });
+  const result = await booked_rooms.create({
+    rno: rno,
+    type: type,
+    beds: beds,
+    sdate: sdate,
+    edate: edate,
+  });
 
-//   res.send("Room =>" + result);
-// });
+  res.send("Room =>" + result);
+});
 
 // temp for adding data to the available rooms for testing
-// app.post("/addDataAvailable", async (req, res) => {
-//   const rno = Number(req.body.rno);
-//   const type = String(req.body.type);
-//   const beds = Number(req.body.beds);
+app.post("/addDataAvailable", async (req, res) => {
+  const rno = Number(req.body.rno);
+  const type = String(req.body.type);
+  const beds = Number(req.body.beds);
 
-//   const result = await available_rooms.create({
-//     rno: rno,
-//     type: type,
-//     beds: beds,
-//   });
+  const result = await available_rooms.create({
+    rno: rno,
+    type: type,
+    beds: beds,
+  });
 
-//   res.send("Room =>" + result);
-// });
+  res.send("Room =>" + result);
+});
 
 // temp for adding new objafter changes
-// app.post("/addDataAvailable", async (req, res) => {
-//   const type = String(req.body.type);
-//   const adults = Number(req.body.adults);
-//   const children = Number(req.body.children);
-//   const available = Number(req.body.available);
-//   const booked = Number(req.body.booked);
+app.post("/addDataAvail", async (req, res) => {
+  const type = String(req.body.type);
+  const adults = Number(req.body.adults);
+  const children = Number(req.body.children);
+  const available = Number(req.body.available);
+  const booked = Number(req.body.booked);
 
-//   const result = await avail_rooms.create({
-//     type: type,
-//     adults: adults,
-//     children: children,
-//     available: available,
-//     booked: booked,
-//   });
+  const result = await avail_rooms.create({
+    type: type,
+    adults: adults,
+    children: children,
+    available: available,
+    booked: booked,
+  });
 
-//   res.send("Room =>" + result);
-// });
+  res.send("Room =>" + result);
+});
 
 // temp for adding new room_booked
-// app.post("/addData", async (req, res) => {
-//   const type = String(req.body.type);
-//   const sdate = new Date(req.body.sdate);
-//   const edate = new Date(req.body.edate);
-//   const adults = Number(req.body.adults);
-//   const children = Number(req.body.children);
+app.post("/addDatabooked", async (req, res) => {
+  const type = String(req.body.type);
+  const sdate = new Date(req.body.sdate);
+  const edate = new Date(req.body.edate);
+  const adults = Number(req.body.adults);
+  const children = Number(req.body.children);
 
-//   const result = await room_booked.create({
-//     type: type,
-//     sdate: sdate,
-//     edate: edate,
-//     adults: adults,
-//     children: children,
-//   });
+  const result = await room_booked.create({
+    type: type,
+    sdate: sdate,
+    edate: edate,
+    adults: adults,
+    children: children,
+  });
 
-//   res.send("Room =>" + result);
-// });
+  res.send("Room =>" + result);
+});
 
 app.listen(port, () => {
   console.log(`server Is Live at http://127.0.0.1:${port}`);
